72b15fadae08424b6c3f9d3e05b6efb352c0d2d9
Theo Markettos <git@markettos.org.uk>
2000-09-01 16:59:49 +0000

    Import of version 0.10 into CVS

---
+++ Sources/vartypes.h
@@ -0,0 +1,20 @@
+#ifndef VARTYPES_C
+#define VARTYPES_C
+/*
+typedef unsigned int uint32;
+typedef signed int sint32;
+typedef unsigned short uint16;
+typedef signed short sint16;
+typedef unsigned char uint8;
+typedef signed char sint8;
+typedef unsigned char uchar;
+typedef signed char schar;
+*/
+
+#define uint32 unsigned int
+#define sint32 signed int
+#define uchar unsigned char
+#define schar signed char
+/*typedef unsigned char bool;*/
+
+#endif
