a0980755525c53358e7ca9bcc51e8f37d1a62000
Theo Markettos <git@markettos.org.uk>
2001-01-26 21:54:32 +0000

    Import of version 0.10 into CVS

---
+++ Sources/version.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void version_printlicence(void)
+{
+  printf("Copyright 2000-1 Theo Markettos\nPortions copyright Simon Tatham, Gary S. Brown and Eric Young\n\n");
+
+  printf("Permission is hereby granted, free of charge, to any person\nobtaining a copy of this software and associated documentation files (the\n\"Software\"), to deal in the Software without restriction, including without\nlimitation the rights to use, copy, modify, merge, publish, distribute,\nsublicense, and/or sell copies of the Software, and to permit persons to whom\nthe Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in\nall copies or substantial portions of the Software.\n\n");
+
+  printf("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL\nSIMON TATHAM OR THEO MARKETTOS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE.\n");
+
+}
